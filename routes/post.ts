fca2f9c81467b93a077d9217e4627e9713e82864
Jorge <joryorch4000@gmail.com>
2019-06-05 20:37:57 -0400

    espacio

--- routes/post.ts
+++ routes/post.ts
@@ -219,7 +219,7 @@ postRoutes.get('/:id', [verificaToken], (req:any, res:Response) => {
        if(!posts){
            return res.status(400).json({
                ok:false,
-               mensaje: `No existe un post con ese Id ${req.params.id}`,
+               mensaje: `No fdfdafdexiste un post con ese Id ${req.params.id}`,
 
                
            })
