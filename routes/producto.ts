ad10468e4d20a774b617978bc4c97c128f382ffa
Jorge <joryorch4000@gmail.com>
2019-06-05 20:34:27 -0400

    espacio

--- routes/producto.ts
+++ routes/producto.ts
@@ -85,51 +85,6 @@ productoRoutes.post('/', [ verificaToken ], (req: any, res: Response) => {
 
 
 
-//******************************************************************************//
-
-//actualizar producto
-
-//******************************************************************************//
-
-
-productoRoutes.get('/:id', [verificaToken], (req:any, res:Response) => {
-
-    
-    Producto.findById(req.params.id)
-   .populate('usuario', '-password')
-   .exec((err,posts)=>{
-
-     
-       if(!posts){
-           return res.status(400).json({
-               ok:false,
-               mensaje: `No existe un post con ese Id ${req.params.id}`,
-
-               
-           })
-       }
-       if(err){
-        return res.status(500).json({
-            ok:false,
-            err
-            
-        })
-    }
-       
-       res.json({
-           ok: true,
-          post: posts
-       });
-   
-   });
-});
-
-
-//******************************************************************************//
-
-
-
-
 
 //******************************************************************************//
 //actualizar producto
