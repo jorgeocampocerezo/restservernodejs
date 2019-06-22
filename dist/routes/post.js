bd90d8b535561c168943f2c049fefdb8bcd23097
Jorge <joryorch4000@gmail.com>
2019-06-21 23:18:58 -0400

    fd

--- dist/routes/post.js
+++ dist/routes/post.js
@@ -258,9 +258,9 @@ postRoutes.get('/postCat/:termino', (req, res) => __awaiter(this, void 0, void 0
     });
 }));
 //eliminar post 
-postRoutes.delete('/borrar/:id', (req, res) => __awaiter(this, void 0, void 0, function* () {
+postRoutes.delete('/borrar/:id', [autenticacion_1.verificaToken], (req, res) => {
     const id = req.params.id;
-    yield post_model_1.Post.findByIdAndRemove(id, (err, poDB) => {
+    post_model_1.Post.findByIdAndRemove(id, (err, poDB) => {
         if (!poDB) {
             return res.json({
                 ok: false,
@@ -275,7 +275,7 @@ postRoutes.delete('/borrar/:id', (req, res) => __awaiter(this, void 0, void 0, f
             mensaje: 'El post fue eliminado'
         });
     });
-}));
+});
 ////**suma categorias */
 postRoutes.get('/sumaCat/:categoria', (req, res) => {
     let categoria = req.params.categoria;
