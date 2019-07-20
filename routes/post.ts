f6478418da411041cbbe0e12a9788501d8a48ddd
jorge <joryorch4000@gmail.com>
2019-07-19 22:37:15 -0400

    fds

--- routes/post.ts
+++ routes/post.ts
@@ -180,6 +180,7 @@ postRoutes.post( '/upload', [ verificaToken ], async (req: any, res: Response) =
 });
 
 
+
 //obtener la imagen del post
 postRoutes.get('/imagen/:userid/:img', (req: any, res: Response) => {
 
