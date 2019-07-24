760ab5dba55c37336930b5b5fb59f5ea0ed70e72
jorge <joryorch4000@gmail.com>
2019-07-24 15:04:16 -0400

    fsd

--- classes/server.ts
+++ classes/server.ts
@@ -7,7 +7,7 @@ export default class Server {
     public app : express.Application;
 //antes    public PORT= process.env.PORT || 3000;
 
-    public PORT= process.env.PORT || 3000;
+    public PORT= process.env.PORT ;
 
     constructor() { 
         this.app = express();
